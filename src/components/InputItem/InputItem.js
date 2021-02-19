accc775b62f2f16f777439a8dd67f229ea6c0d1d
Anastasia <62800962+ananastya77@users.noreply.github.com>
2021-02-19 20:38:14 +0300

    Add app structure files

---
+++ src/components/InputItem/InputItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const InputItem = () => (<div>
+    <p>Input Your Task</p>
+  </div>);
+
+export default InputItem;
---
+++ src/components/Item/Item.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Item = () => (<span>Important Task!</span>);
+
+export default Item;
