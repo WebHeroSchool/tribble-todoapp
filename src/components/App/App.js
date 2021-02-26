aff111ebe4ac0dd9b221cf029cf622508d2bf0ac
Anastasia <62800962+ananastya77@users.noreply.github.com>
2021-02-26 13:30:10 +0300

    resolve conflict

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,4 @@ import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 
-const todoItem = 'Пройти новый урок';
-
-const App = () => (<div>
-  <h1>your todos</h1>
-  <InputItem />
-  <ItemList todoItem={todoItem}/>
-  <Footer count={3}/>
-</div>);
-
 export default App;
