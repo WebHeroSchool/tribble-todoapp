1213eff3bc1f4f26327f972ca6ffd6359c91570c
Anastasia <62800962+ananastya77@users.noreply.github.com>
2021-02-26 13:36:33 +0300

    Resolve conflict

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,5 +2,22 @@ import React from 'react';
 import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
+import './App.css';
+
+const App = () => {
+  const items = [
+    { value: 'Пройти новый урок' },
+    { value: 'Сделать зарядку' },
+    { value: 'Написать приложение' }
+  ];
+
+  return (
+    <div className="wrap">
+      <h1 className="wrap__title">your todos</h1>
+      <InputItem />
+      <ItemList items={items}/>
+      <Footer count={3}/>
+    </div>
+)};
 
 export default App;
