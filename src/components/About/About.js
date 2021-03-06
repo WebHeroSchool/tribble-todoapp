abee1c90353f3df6ee2d4890501d9dd90af41971
Anastasia <62800962+ananastya77@users.noreply.github.com>
2021-03-06 16:13:26 +0300

    Add menu & routing

---
+++ src/components/About/About.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const About = () => (
+  <div>
+    <h1>About</h1>
+  </div>
+);
+
+export default About;
---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const Contacts = () => (
+  <div>
+    <h1>Contacts</h1>
+  </div>
+);
+
+export default Contacts;
